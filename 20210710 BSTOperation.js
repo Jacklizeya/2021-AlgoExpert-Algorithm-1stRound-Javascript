7e0e14a0199b30a559b023312a3e1e892a0e49d5
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-07-10 19:21:22 -0600

    Update 20210710 BSTOperation.js

--- 20210710 BSTOperation.js
+++ 20210710 BSTOperation.js
@@ -1,4 +1,4 @@
-// This is the code pass 60% of the test
+// This is the code pass 70% of the test
 
 // Do not edit the class below except for
 // the insert, contains, and remove methods.
@@ -14,9 +14,8 @@ class BST {
   insert(value) {
     // Write your code here.
     // Do not edit the return statement of this method.
-		console.log("insert", this, value)
 		if (value < this.value && this.left !== null) {this.left.insert(value);}
-		if (value < this.value && this.left === null) {console.log("fit this condition"); this.left = new BST(value); }
+		if (value < this.value && this.left === null) {this.left = new BST(value); }
 		if (value >= this.value && this.right !== null) {this.right.insert(value); }
 		if (value >= this.value && this.right === null) {this.right = new BST (value); }
     return this;
@@ -35,25 +34,32 @@ class BST {
   remove(value) {
     // Write your code here.
     // Do not edit the return statement of this method.
-		console.log("remove", value)
+		console.log("********remove", this, "remove", value)
 		
 		if (value === this.value && this.right !== null && this.left !== null) {
+			console.log("this", this.value)
 			let previous = this
 			let current = this.right
 			while (current.left !== null) {
 				previous = current
 				current = current.left
 			}
+			console.log("+++++, previous, current", previous, current)
 			this.value = current.value
+			console.log("success47, this.value")
 			previous.left = current.right
+			console.log("success49", previous.left)
+			
 			}
-		if (value === this.value && this.right !== null  && this.left === null) {this.value = this.right.value; this.right = this.right.right; this.left = this.right.left }
+		if (value === this.value && this.right !== null  && this.left === null) {this.value = this.right.value; this.left = this.right.left; this.right = this.right.right;  }
 		if (value === this.value && this.right === null  && this.left !== null) {this.value = this.left.value; this.right = this.left.right; this.left = this.left.left}
 		if (value === this.value && this.right === null  && this.left === null) {this.value = null; this.left = null; this.right = null}
 		if (value < this.value && this.left !== null) { this.left.remove(value); }
 		if (value < this.value && this.left === null) {}
 		if (value > this.value && this.right !== null) {this.right.remove(value); }
 		if (value > this.value && this.right === null) {}
+		
+		console.log("new this", this)
     return this;
   }
 }
