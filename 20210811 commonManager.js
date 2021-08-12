080d0ee0267457cd9a5c8a704a3675b5e4a698dd
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-08-11 19:49:22 -0600

    Create 20210811 commonManager.js

---
+++ 20210811 commonManager.js
@@ -0,0 +1,56 @@
+function getLowestCommonManager(topManager, reportOne, reportTwo) {
+  // Write your code here.
+	  let Array1
+		findReportOne(topManager, reportOne, [topManager])
+		console.log(Array1)
+	
+		let Array2
+		findReportTwo(topManager, reportTwo, [topManager])
+		console.log(Array2)
+	
+	  for (i= Array1.length - 1; i >=0; i--) {
+			if (Array2.includes(Array1[i])) {console.log(Array1[i]); return Array1[i]}
+		}
+	
+	function findReportOne(topManager, reportOne, path = [topManager.name]) {
+	// console.log(topManager.name, reportOne.name, path)
+	if (topManager.name === reportOne.name) {
+		Array1 = path}	
+		
+	if (topManager.directReports.length === 0) {return}
+	
+	topManager.directReports.forEach(report => {
+		let newpath = [...path]
+		newpath.push(report)
+		findReportOne(report, reportOne, newpath)})
+}	
+
+	
+	function findReportTwo(topManager, reportTwo, path = [topManager.name]) {
+	// console.log(topManager.name, reportTwo.name, path)
+	if (topManager.name === reportTwo.name) {
+		Array2 = path}	
+		
+	if (topManager.directReports.length === 0) {return}
+	
+	topManager.directReports.forEach(report => {
+		let newpath = [...path]
+		newpath.push(report)
+		findReportTwo(report, reportTwo, newpath)})
+}	
+
+}
+
+
+
+
+// This is an input class. Do not edit.
+class OrgChart {
+  constructor(name) {
+    this.name = name;
+    this.directReports = [];
+  }
+}
+
+// Do not edit the line below.
+exports.getLowestCommonManager = getLowestCommonManager;
